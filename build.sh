faf7282a3657bf75ad2ab097f1e5549c529bc2c8
Jonas Levis <s80programmeomega@gmail.com>
2026-01-10 21:29:53 +0100

    Add management commands to create admin user and seed dialogues

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py create_admin
+python manage.py seed_dialogues
+python manage.py create_sample_data
